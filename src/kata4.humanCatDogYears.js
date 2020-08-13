309c817a54aaf665ee6692c74bcdd4c29f2ab13e
Alan Brooks <abrooks@live.co.uk>
2020-08-14 00:23:17 +0100

    kata 4 complete

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,19 @@
-const humanCatDogYears = number => {};
-
+const humanCatDogYears = number => {;
+    let catYears = 0;
+    let dogYears = 0;
+    if (number === 1) {
+        catYears = dogYears = number + 14;
+    }
+    else if (number === 2) {
+        catYears = dogYears = number + 22;
+    }
+    else if (number > 2) {
+        const numYearsOverTwo = number - 2;
+        catYears = 24 + numYearsOverTwo * 4;
+        dogYears = 24 + numYearsOverTwo * 5;
+    }
+    let arrayOfYears = [];
+    arrayOfYears.push(number, catYears, dogYears);
+    return arrayOfYears;
+}
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require("../src");
-
+    
+    describe("Human years to cat and dog years via array", () => {
+      test("returns cat and dog years based on human years", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+      });
+    });
 // Look Ma, no handlebars!!!
