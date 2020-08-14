f76c8620694e191f2a7a6580b63931612a4d8f1c
Alan Brooks <abrooks@live.co.uk>
2020-08-15 00:12:40 +0100

    kata 6 complete - loopy

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = namesObj => {};
-
+const joinNames = namesObj => {;
+    let nameString = "";
+    for (let i = 0; i < namesObj.length - 1; i++){
+        if (i != namesObj.length -2) {
+            nameString += (namesObj[i].name + ", ");}
+        else {
+            nameString += (namesObj[i].name + " & ")
+        }
+    }
+    nameString += (namesObj[namesObj.length - 1].name);
+    return nameString;
+}
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -2,4 +2,9 @@ const { joinNames } = require("../src");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {});
+    expect(joinNames([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' }])).toBe('Bart, Lisa & Maggie')
+
+    test("returns string of names, seperated by commas and an ampersand", () => {});
+    expect(joinNames([{ name: 'Bart' }, { name: 'Lisa' }, { name: 'Maggie' },  {name: 'Homer' }])).toBe('Bart, Lisa, Maggie & Homer')
+
 });
