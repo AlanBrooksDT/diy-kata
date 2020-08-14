3271e8c8e042c1ea0762ad3e93d2458d31712ba1
Alan Brooks <abrooks@live.co.uk>
2020-08-14 01:36:13 +0100

    kata 5 complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,5 @@
-const reachDestination = (distance, speed) => {};
-
+const reachDestination = (distance, speed) => {
+   let timeToArrive = Math.ceil((distance / speed)*2)/2;
+   return ("I should be there in " + timeToArrive + " hours");
+};
 module.exports = reachDestination;
